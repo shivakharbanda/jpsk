0a7a9b8e3fcff4f50acaf325ba40a2c7d4d59dcf
Bavid Lynx <shivakharbanda11@gmail.com>
2020-07-03 23:51:35 +0530

    added everything

---
+++ build/precache-manifest.fe4b763e8dc1bf55380f47493aa413de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d57e1e1ba932987e4fea",
+    "url": "/static/css/main.28d2a218.chunk.css"
+  },
+  {
+    "revision": "d57e1e1ba932987e4fea",
+    "url": "/static/js/main.d57e1e1b.chunk.js"
+  },
+  {
+    "revision": "d513daa49de2a5367dbf",
+    "url": "/static/css/1.8f32bfb5.chunk.css"
+  },
+  {
+    "revision": "d513daa49de2a5367dbf",
+    "url": "/static/js/1.d513daa4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b92b96d2a11b23f372931d24c81f225c",
+    "url": "/index.html"
+  }
+];
